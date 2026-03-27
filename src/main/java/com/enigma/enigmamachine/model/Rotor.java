5dfa360c20e05b52c8ba1d3da203dec69ba7474d
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2026-03-27 09:51:43 +0100

    Document rotor wirings in Rotor class
    Add inline comments to src/main/java/com/enigma/enigmamachine/model/Rotor.java listing the wiring strings and notch positions for Rotors I, II, and III (Italian 'Tacca su' notation). This documents the rotor configurations to aid future implementation of wiring and notch/stepping behavior.

--- src/main/java/com/enigma/enigmamachine/model/Rotor.java
+++ src/main/java/com/enigma/enigmamachine/model/Rotor.java
@@ -1,4 +1,8 @@
 package com.enigma.enigmamachine.model;
 
 public class Rotor {
+
+    //Rotore I: EKMFLGDQVZNTOWYHXUSPAIBRCJ (Tacca su Q)
+    //Rotore II: AJDKSIRUXBLHWTMCQGZNPYFVOE (Tacca su E)
+    //Rotore III: BDFHJLCPDSXONYTQVZWJKRMAPU (Tacca su V)
 }
