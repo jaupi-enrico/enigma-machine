1092be85e8e3a6319f2fc68104d82bfd07c3b320
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2026-03-28 09:51:39 +0100

    Add rotor wirings, notches and position field
    Introduce constants for three rotor wirings (Alfabeto1, Alfabeto2, Alfabeto3) and their notch indicators (Tacca1, Tacca2, Tacca3). Add posizioneAttuale field to hold the rotor's current position and remove the previous inline comments. Prepares the Rotor model for implementing wiring and stepping logic.

--- src/main/java/com/enigma/enigmamachine/model/Rotor.java
+++ src/main/java/com/enigma/enigmamachine/model/Rotor.java
@@ -1,8 +1,14 @@
 package com.enigma.enigmamachine.model;
 
 public class Rotor {
+    private static final String Alfabeto1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+    private static final String Alfabeto2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+    private static final String Alfabeto3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
 
-    //Rotore I: EKMFLGDQVZNTOWYHXUSPAIBRCJ (Tacca su Q)
-    //Rotore II: AJDKSIRUXBLHWTMCQGZNPYFVOE (Tacca su E)
-    //Rotore III: BDFHJLCPDSXONYTQVZWJKRMAPU (Tacca su V)
+    private static final String Tacca1 = "Q";
+    private static final String Tacca2 = "E";
+    private static final String Tacca3 = "V";
+
+
+    private int posizioneAttuale;
 }
