91ac004e8d12ca0d83a554135b6abda0e4e47d0c
armaanmultani2008 <armaanmultani2008@gmail.com>
2026-03-28 09:49:54 +0100

    Add reflector fields and UKW_B wiring constant
    Introduce private fields to Reflector: an 'input' int and a 'cablaggio' int array, and add the UKW_B static constant string ("YRUHQSLDPXNGOKMIEBFZCWVJAT") representing the reflector wiring. Prepares the class for implementing reflector mapping/lookup logic.

--- src/main/java/com/enigma/enigmamachine/model/Reflector.java
+++ src/main/java/com/enigma/enigmamachine/model/Reflector.java
@@ -1,4 +1,7 @@
 package com.enigma.enigmamachine.model;
 
 public class Reflector {
+    private int input;
+    private static final String UKW_B = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    private int cablaggio[];
 }
