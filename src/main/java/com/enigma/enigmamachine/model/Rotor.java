d6c46ee6dec129e9070130e48a48b6d917684ac4
jaupi-enrico <78313537@itiscastelli.onmicrosoft.com>
2026-03-28 23:13:23 +0100

    Aggiunta della classe rotori e dei test per essa.

--- src/main/java/com/enigma/enigmamachine/model/Rotor.java
+++ src/main/java/com/enigma/enigmamachine/model/Rotor.java
@@ -1,14 +1,88 @@
 package com.enigma.enigmamachine.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Rotor {
-    private static final String Alfabeto1 = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
-    private static final String Alfabeto2 = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
-    private static final String Alfabeto3 = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
-
-    private static final String Tacca1 = "Q";
-    private static final String Tacca2 = "E";
-    private static final String Tacca3 = "V";
-
-
+    private Map<Character, Character> mappaDavanti;
+    private Map<Character, Character> mappaIndietro;
+    private int sizeAlfabeto;
+    private char letteraAvanzamento;
     private int posizioneAttuale;
+
+    public Rotor(String cablaggio, char letteraAvanzamento, int posizione) {
+        this.letteraAvanzamento = letteraAvanzamento;
+        this.sizeAlfabeto  = cablaggio.length();
+        setPosizioneAttuale(posizione);
+
+        mappaDavanti = new HashMap<>();
+        mappaIndietro = new HashMap<>();
+
+        for (int i = 0; i < sizeAlfabeto; i++) {
+            char input = (char) ('A' + i);
+            char output = cablaggio.charAt(i);
+
+            mappaDavanti.put(input, output);
+            mappaIndietro.put(output, input);
+        }
+    }
+
+    public void ruota() {
+        posizioneAttuale = (posizioneAttuale + 1) % sizeAlfabeto;
+    }
+
+    public boolean isLetteraAvanzamento() {
+        return (char) ('A' + posizioneAttuale) == letteraAvanzamento;
+    }
+
+    public char cifraAvanti(char c) {
+        c = Character.toUpperCase(c);
+        int spostamento = (c - 'A' + posizioneAttuale) % sizeAlfabeto;
+        char carattereSpostato = (char) ('A' + spostamento);
+
+        Character carattereMappato = mappaDavanti.get(carattereSpostato);
+        if (carattereMappato == null) {
+            throw new IllegalArgumentException("Carattere non valido: " + c);
+        }
+
+        int output = (carattereMappato - 'A' - posizioneAttuale + sizeAlfabeto) % sizeAlfabeto;
+
+        return (char) ('A' + output);
+    }
+
+    public char cifraIndietro(char c) {
+        c = Character.toUpperCase(c);
+
+        int spostamento = (c - 'A' + posizioneAttuale) % sizeAlfabeto;
+        char carattereSpostato = (char) ('A' + spostamento);
+
+        Character carattereMappato = mappaIndietro.get(carattereSpostato);
+        if (carattereMappato == null) {
+            throw new IllegalArgumentException("Carattere non valido: " + c);
+        }
+
+        int output = (carattereMappato - 'A' - posizioneAttuale + sizeAlfabeto) % sizeAlfabeto;
+
+        return (char) ('A' + output);
+    }
+
+    public int getPosizioneAttuale() {
+        return posizioneAttuale;
+    }
+
+    public void setPosizioneAttuale(int posizioneAttuale) {
+        this.posizioneAttuale = (posizioneAttuale % sizeAlfabeto + sizeAlfabeto) % sizeAlfabeto;
+    }
+
+    static public Rotor creaRotore1(int pos) {
+        return  new Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q', pos);
+    }
+
+    static public Rotor creaRotore2(int pos) {
+        return  new Rotor("AJDKSIRUXBLHWTMCQGZNPYFVOE", 'E', pos);
+    }
+
+    static public Rotor creaRotore3(int pos) {
+        return  new Rotor("BDFHJLCPRTXVZNYEIWGAKMUSQO", 'V', pos);
+    }
 }
---
+++ src/test/java/com/enigma/enigmamachine/model/RotorTest.java
@@ -0,0 +1,21 @@
+package com.enigma.enigmamachine.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class RotorTest {
+
+    @Test
+    void cifratura_decifratura() {
+        Rotor r = new Rotor("EKMFLGDQVZNTOWYHXUSPAIBRCJ", 'Q', 0);
+
+        for (char c = 'A'; c <= 'Z'; c++) {
+            char enc = r.cifraAvanti(c);
+            char dec = r.cifraIndietro(enc);
+
+            if (c != dec) {
+                System.out.println("Errore su: " + c);
+            }
+        }
+    }
+}
