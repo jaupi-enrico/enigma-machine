309fe03c6c9a32e88259d98ba67fda4e8a61be98
armaanmultani2008 <armaanmultani2008@gmail.com>
2026-04-06 19:33:47 +0200

    Update Reflector.java

--- src/main/java/com/enigma/enigmamachine/model/Reflector.java
+++ src/main/java/com/enigma/enigmamachine/model/Reflector.java
@@ -1,7 +1,38 @@
 package com.enigma.enigmamachine.model;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Reflector {
-    private int input;
-    private static final String UKW_B = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
-    private int cablaggio[];
+
+    // mappa del riflettore UKW_B: "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+    // mappa del riflettore UKW-C: "FVPJIAOYEDRZXWGCTKUQSBNMHL"
+
+    private Map<Character, Character> mappaSpecchio;
+    private int sizeAlfabeto;
+
+    public Reflector(String cablaggio){
+        this.sizeAlfabeto = cablaggio.length();
+        this.mappaSpecchio = new HashMap<>();
+
+        for (int i = 0; i < sizeAlfabeto; i++) {
+            char input = (char) ('A' + i);
+            char output = cablaggio.charAt(i);
+
+            mappaSpecchio.put(input, output);
+        }
+    }
+
+    public char rifletti(char c){
+        c = Character.toUpperCase(c);
+        return mappaSpecchio.get(c);
+    }
+
+    static public Reflector creaUKW_B(){
+        return new Reflector("YRUHQSLDPXNGOKMIEBFZCWVJAT");
+    }
+
+    static public Reflector creaUKW_C(){
+        return new Reflector("FVPJIAOYEDRZXWGCTKUQSBNMHL");
+    }
 }
