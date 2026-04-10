b93a8e009efccab1a8a082f637198460a1f075ef
armaanmultani2008 <armaanmultani2008@gmail.com>
2026-04-10 09:15:00 +0200

    aggiunta metodi

--- src/main/java/com/enigma/enigmamachine/EnigmaController.java
+++ src/main/java/com/enigma/enigmamachine/EnigmaController.java
@@ -3,6 +3,7 @@ package com.enigma.enigmamachine;
 import com.enigma.enigmamachine.model.EnigmaEngine;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
 
@@ -11,12 +12,22 @@ public class EnigmaController {
 
     @FXML
     private GridPane gridButton;
+    @FXML
+    private GridPane gridLight;
+    @FXML
+    private GridPane gridPlugBoard;
 
     private Button[] buttons;
+    private Label[] labels;
     @FXML
     void initialize() {
         macchina = new EnigmaEngine();
 
+        creaGridButton();
+        creaGridPlugBoard();
+    }
+
+    private void creaGridButton(){
         //Crea il vettore di bottoni
         buttons = new Button[26];
         //Inizializza alla prima lettera, cioè la A
@@ -43,6 +54,55 @@ public class EnigmaController {
                 });
                 //Aggiunge il bottone alla griglia nella posizione corrispondente
                 gridButton.add(buttons[i * 9 + j], j, i, 1, 1);
+                //Incrementa la lettera per passare alla successiva
+                lettera++;
+                //Se la lettera è arrivata alla Z, esce dal ciclo
+                //Ovviamente si potrebbe anche aggiungere ad esempio il carattere per lo spazio
+                //ma, per quanto detto, non verrebbe usato
+                if (lettera == '[') return;
+            }
+        }
+    }
+
+    private void creaGridPlugBoard(){
+        gridPlugBoard.setHgap(10);
+        gridPlugBoard.setVgap(10);
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 9; j++) {;
+                //Variabile finale per poter essere usata all'interno della lambda
+                final char finalLettera;
+                //Aggiunge un evento al bottone che stampa la lettera corrispondente quando viene premuto
+                //Il bottone viene identificato tramite l'evento e la lettera viene stampata tramite la variabile finale
+                //Nella versione definitiva, invece di stampare la lettera, verrà chiamato il metodo per criptare la lettera e visualizzare il risultato
+                buttons[i * 9 + j].setOnAction(e -> {
+
+                });
+                //Aggiunge il bottone alla griglia nella posizione corrispondente
+                gridPlugBoard.add(buttons[i * 9 + j], j, i, 1, 1);
+            }
+        }
+    }
+
+    private void creaGridLight(){
+        //Crea il vettore di bottoni
+        labels = new Label[26];
+        //Inizializza alla prima lettera, cioè la A
+        char lettera = 'A';
+        //Imposta le dimensioni dei separatori della griglia
+        gridLight.setHgap(10);
+        gridLight.setVgap(10);
+        //Scorre ogni riga della tastiera
+        for (int i = 0; i < 3; i++) {
+            //Per ogni riga aggiunge 9 bottoni, fino ad arrivare alla Z
+            for (int j = 0; j < 9; j++) {
+                //Crea il nuovo bottone con la lettera corrispondente
+                labels[i * 9 + j] = new Label("" + lettera);
+                //Stabilisce la dimensione del bottone in modo che occupi 1/10 dello spazio disponibile
+                labels[i * 9 + j].setPrefWidth(800.0 / 10);
+                //Variabile finale per poter essere usata all'interno della lambda
+                final char finalLettera = lettera;
+                //Aggiunge il bottone alla griglia nella posizione corrispondente
+                gridLight.add(labels[i * 9 + j], j, i, 1, 1);
                 //Incrementa la lettera per passare alla successiva
                 lettera++;
                 //Se la lettera è arrivata alla Z, esce dal ciclo
